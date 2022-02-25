9a0331052238ef61b06023f8908443044872151c
BuckLuckDuck <nikitos80011@gmail.com>
2022-02-25 20:05:13 +0700

    1.0 with comments

--- Form1.cs
+++ Form1.cs
@@ -16,13 +16,21 @@ namespace Simulation_Lab_3
         {
             InitializeComponent();
         }
-
-        // 111 110 101 100 011 010 001 000
+        
+        // Массив значение 2^3, для поиска позиций в правилах.
         string[] positions = new string[] { "111", "110", "101", "100", "011", "010", "001", "000" };
+        // Число "правил", преобразованное в двоичную форму и разбитое на символьный массив.
         char[] cellRules;
-        bool start = true; /// Запрет воздействия на ячейки, после нажатия кнопки старт.
+        // Запрет воздействия на ячейки, после нажатия кнопки старт.
+        bool start = true; 
+        // Переменная указывающая количество используемых строк.
         int rowCounter = 0;
 
+        /// <summary>
+        /// Функция преобразования десятичного числа "правил" в двоичную форму.
+        /// </summary>
+        /// <param name="rule">Десятичное число "правил".</param>
+        /// <returns>Массив символов размером 2^3, обозначающий правила по позициям из массива "positions".</returns>
         private char[] acceptRules(int rule)
         {
             char[] result;
@@ -43,6 +51,11 @@ namespace Simulation_Lab_3
             return result;
         }
 
+        /// <summary>
+        /// Функция поиска "правила" по найденной комбинации.
+        /// </summary>
+        /// <param name="xyz">Комбинация.</param>
+        /// <returns>Значение комбинации по "правилам."</returns>
         private char calculateLayerCellValue(char[] xyz)
         {
             char result;
@@ -58,49 +71,58 @@ namespace Simulation_Lab_3
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
-            nudColumnsCount.Enabled = false;
-            nudRules.Enabled = false;
-            btnStart.Enabled = true;
+            // При нажатие на кнопку "Create".
+            nudColumnsCount.Enabled = false; // Отключается возможность редактирования параметров
+            nudRules.Enabled = false;        // количества колонок и указания новых правил.
+            btnStart.Enabled = true;         // Появляется возможность нажать кнопку "Start".
 
-            dataGridView.Rows.Clear();                      
+            dataGridView.Rows.Clear();       // Очищается вся таблица
 
+            // Удаляются все колонки
             for (int i = 0; i < dataGridView.Columns.Count; i++)                 
             {
                 dataGridView.Columns.RemoveAt(0);
             }
 
+            // Добавляется количество колонок, запрошенное пользователем.
             for (int i = 0; i < (int)nudColumnsCount.Value; i++)
             {
                 dataGridView.Columns.Add("","");
             }
             
+            // Добавляется первичная строка, для пользовательского взаимодействия.
             dataGridView.Rows.Add();
 
+            // Утверждаются правила в массив "cellRules".
             int rule = (int)nudRules.Value;
             cellRules = acceptRules(rule);
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            btnCreate.Enabled = false;
-            btnStop.Enabled = true;
-            btnStart.Enabled = false;
+            // При нажатие на кнопку "Start".
+            btnCreate.Enabled = false;  // Отключается возможность повторного создания полей.
+            btnStop.Enabled = true;     // Появляется возможность остановить компиляцию программы.
+            btnStart.Enabled = false;   // Отключается возможность повторного старта программы.
 
-            start = false;
+            start = false;              // Вводится запрет на изменение ячеек пользователем.
 
-            timer1.Start();
+            timer1.Start();             // Компиляция основного кода программы.
         }
         private void btnStop_Click(object sender, EventArgs e)
         {
-            timer1.Stop();
-            btnStart.Enabled = false;
-            btnCreate.Enabled = true;
-            btnStop.Enabled = false;
-            start = true;
-            nudColumnsCount.Enabled = true;
+            // При нажатие на кнопку "Stop".
+            timer1.Stop();                      // Остановка компиляции.
+            btnCreate.Enabled = true;           // Появляется возможность заного задать поле и правила.
+            btnStop.Enabled = false;            // Отключается возможность повторного нажатия кнопки.
+            start = true;                       // Пользователь снова может взаимодействовать с полями.
+            nudColumnsCount.Enabled = true;     // Появляется возможность задать новые параметры поля и правил.
             nudRules.Enabled = true;
         }
 
+        /// <summary>
+        /// Возможность покраски ячеек левой и правой кнопками мыши, вне компиляции программы.
+        /// </summary>
         private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             if (start)
@@ -117,6 +139,9 @@ namespace Simulation_Lab_3
             }
         }
 
+        /// <summary>
+        /// Отключение функции выделения ячеек.
+        /// </summary>
         private void dataGridView_SelectionChanged(object sender, EventArgs e)
         {
             try
@@ -126,21 +151,32 @@ namespace Simulation_Lab_3
             catch { }
         }
 
+        /// <summary>
+        /// Основной процесс моделирования.
+        /// </summary>
         private void timer1_Tick(object sender, EventArgs e)
         {
+            // Массив предыдущих значений поколения.
             char[] previousLayer = new char[dataGridView.Columns.Count];
+
+            // Массив будущих значений поколения.
             char[] currentLayer = new char[dataGridView.Columns.Count];
+
+            // Комбинация для вычисления будущего значения ячейки.
             char[] xyz = new char[3];
             
+            // Заполнение массива предыдущих значений.
             for (int i = 0; i < previousLayer.Length; i++)
             {
                 if (dataGridView[i, rowCounter].Style.BackColor == Color.Red) previousLayer[i] = '1';
                 else previousLayer[i] = '0';
             }
 
+            // Добавления новой строки, для нового поколения.
             dataGridView.Rows.Add();
             rowCounter++;
 
+            // Вычисление значений нового поколения на основе предыдущих значений.
             for (int i = 0; i < currentLayer.Length;i++)
             {
                 if (i == 0 | i == currentLayer.Length - 1)
@@ -163,6 +199,7 @@ namespace Simulation_Lab_3
                 currentLayer[i] = calculateLayerCellValue(xyz);
             }
 
+            // Покраска ячеек в новосозданной строке.
             for (int i = 0; i < currentLayer.Length; i++)
             {
                 if (currentLayer[i] == '0') dataGridView[i, rowCounter].Style.BackColor = Color.White;
