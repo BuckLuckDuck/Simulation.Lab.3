394e3c1540a7da8f2b31c3a6c953211a2db4bbd7
BuckLuckDuck <nikitos80011@gmail.com>
2022-02-25 17:40:47 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Simulation_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudRules = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudColumnsCount = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRules)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumnsCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnStart);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.nudRules);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.nudColumnsCount);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView);
+            this.splitContainer1.Size = new System.Drawing.Size(1190, 563);
+            this.splitContainer1.SplitterDistance = 280;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(35, 172);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(119, 44);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(32, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rules";
+            // 
+            // nudRules
+            // 
+            this.nudRules.Location = new System.Drawing.Point(35, 121);
+            this.nudRules.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudRules.Name = "nudRules";
+            this.nudRules.Size = new System.Drawing.Size(119, 20);
+            this.nudRules.TabIndex = 2;
+            this.nudRules.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudRules.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Columns count";
+            // 
+            // nudColumnsCount
+            // 
+            this.nudColumnsCount.Location = new System.Drawing.Point(35, 57);
+            this.nudColumnsCount.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudColumnsCount.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.nudColumnsCount.Name = "nudColumnsCount";
+            this.nudColumnsCount.Size = new System.Drawing.Size(119, 20);
+            this.nudColumnsCount.TabIndex = 0;
+            this.nudColumnsCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudColumnsCount.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.ColumnHeadersVisible = false;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView.Size = new System.Drawing.Size(902, 559);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 563);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudRules)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumnsCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudRules;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudColumnsCount;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 111 110 101 100 011 010 001 000
+        string[] positions = new string[] { "111", "110", "101", "100", "011", "010", "001", "000" };
+        char[] cellRules;
+
+        private char[] acceptRules(int rule)
+        {
+            char[] result;
+
+            string binaryCode = Convert.ToString(rule, 2);
+            
+            if (binaryCode.Length != 8)
+            {
+                for (int i = 0; i < 8 - binaryCode.Length; i++)
+                {
+                    binaryCode = "0" + binaryCode;
+                }
+            }
+            
+            result = binaryCode.ToCharArray();
+
+            return result;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            dataGridView.Rows.Clear();                      
+            int count = dataGridView.Columns.Count;
+            for (int i = 0; i < count; i++)                 
+            {
+                dataGridView.Columns.RemoveAt(0);
+            }
+            int columnsCount = (int)nudColumnsCount.Value;
+            for (int i = 0; i < columnsCount; i++)
+            {
+                dataGridView.Columns.Add("","");
+            }
+            
+            dataGridView.Rows.Add();
+            int rule = (int)nudRules.Value;
+            cellRules = acceptRules(rule);
+        }
+
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    dataGridView[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Red;
+                    dataGridView.ClearSelection();
+                    break;
+                case MouseButtons.Right:
+                    dataGridView[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.White;
+                    dataGridView.ClearSelection();
+                    break;
+            }
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ((DataGridView)sender).SelectedCells[0].Selected = false;
+            }
+            catch { }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
