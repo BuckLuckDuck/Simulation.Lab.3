09daede263eefb8ae728d1114e2a588f1cb41fda
BuckLuckDuck <nikitos80011@gmail.com>
2022-02-25 19:07:29 +0700

    add adding row and calculate layers

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -167,6 +167,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // btnStart
--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,9 @@ namespace Simulation_Lab_3
         // 111 110 101 100 011 010 001 000
         string[] positions = new string[] { "111", "110", "101", "100", "011", "010", "001", "000" };
         char[] cellRules;
-        bool start = true;
+        bool start = true; /// Запрет воздействия на ячейки, после нажатия кнопки старт.
+        int rowCounter = 0;
+        int colCounter = 0;
 
         private char[] acceptRules(int rule)
         {
@@ -41,19 +43,32 @@ namespace Simulation_Lab_3
             return result;
         }
 
+        private char calculateLayerCellValue(char[] xyz)
+        {
+            char result;
+
+            string code = new string(xyz);
+
+            int index = Array.IndexOf(xyz, positions);
+
+            result = cellRules[index];
+
+            return result;
+        }
+
         private void btnCreate_Click(object sender, EventArgs e)
         {
-            dataGridView.Rows.Clear();                      
-            int count = dataGridView.Columns.Count;
+            nudColumnsCount.Enabled = false;
+            nudRules.Enabled = false;
 
-            for (int i = 0; i < count; i++)                 
+            dataGridView.Rows.Clear();                      
+
+            for (int i = 0; i < dataGridView.Columns.Count; i++)                 
             {
                 dataGridView.Columns.RemoveAt(0);
             }
 
-            int columnsCount = (int)nudColumnsCount.Value;
-
-            for (int i = 0; i < columnsCount; i++)
+            for (int i = 0; i < (int)nudColumnsCount.Value; i++)
             {
                 dataGridView.Columns.Add("","");
             }
@@ -66,12 +81,11 @@ namespace Simulation_Lab_3
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            nudColumnsCount.Enabled = false;
-            nudRules.Enabled = false;
             btnCreate.Enabled = false;
+            
             start = false;
 
-            
+            timer1.Start();
         }
 
         private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -101,6 +115,43 @@ namespace Simulation_Lab_3
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            char[] previousLayer = new char[dataGridView.Columns.Count];
+            char[] currentLayer = new char[dataGridView.Columns.Count];
+            char[] xyz = new char[3];
+            
+            rowCounter = 0;
+            colCounter = 0;
+
+            for (int i = 0; i < previousLayer.Length; i++)
+            {
+                if (dataGridView[i, rowCounter].Style.BackColor == Color.Red) previousLayer[i] = '1';
+                else previousLayer[i] = '0';
+            }
+
+            dataGridView.Rows.Add();
+
+            for (int i = 0; i < currentLayer.Length;i++)
+            {
+                if (i == 0 | i == currentLayer.Length - 1)
+                {
+                    if (i == 0) xyz[0] = previousLayer[currentLayer.Length - 1];
+                    else xyz[0] = previousLayer[i - 1];
+                    xyz[1] = previousLayer[i];
+                    if (i == 0) xyz[2] = previousLayer[i + 1];
+                    else xyz[2] = previousLayer[0];
+
+                    currentLayer[i] = calculateLayerCellValue(xyz);
+
+                    continue;
+                }
+
+                xyz[0] = previousLayer[i - 1];
+                xyz[1] = previousLayer[i];
+                xyz[2] = previousLayer[i + 1];
+
+                currentLayer[i] = calculateLayerCellValue(xyz);
+            }
+
 
         }
     }
